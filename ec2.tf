6d7f18f58e02bfa84406825962e18707c3a8d481
Emil Furniga <emil.furniga@gmail.com>
2024-10-29 21:53:37 +0200

    Enable web and app tier ec2 instances

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "PublicWebTemplate" {
 #### EC2 instance Web Tier ###
 ##############################
 
-/*resource "aws_instance" "PublicWebTemplate" {
+resource "aws_instance" "PublicWebTemplate" {
   ami                    = "ami-052efd3df9dad4825"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public-web-subnet-1.id
@@ -51,7 +51,7 @@ resource "aws_instance" "PublicWebTemplate" {
   tags = {
     Name = "web-asg"
   }
-}*/
+}
 
 
 resource "aws_instance" "private-app-template" {
@@ -70,7 +70,7 @@ resource "aws_instance" "private-app-template" {
 #### EC2 instance App Tier ###
 ##############################
 
-/*resource "aws_instance" "private-app-template" {
+resource "aws_instance" "private-app-template" {
   ami                    = "ami-052efd3df9dad4825"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private-app-subnet-1.id
@@ -80,8 +80,4 @@ resource "aws_instance" "private-app-template" {
   tags = {
     Name = "app-asg"
   }
-}*/
-
-
-
-
+}
