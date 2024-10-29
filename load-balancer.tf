a9320838ba117ea9a5c38df909c4928e453a3633
Emil Furniga <emil.furniga@gmail.com>
2024-10-29 21:43:23 +0200

    Modify loadbalancers to add HTTPS

--- load-balancer.tf
+++ load-balancer.tf
@@ -47,7 +47,7 @@ resource "aws_lb_listener" "alb_http_listener" {
 
 
 # create a listener on port 443 with forward action
-/*resource "aws_lb_listener" "alb_https_listener" {
+resource "aws_lb_listener" "alb_https_listener" {
   load_balancer_arn = aws_lb.application-load-balancer.arn
   port              = 443
   protocol          = "HTTPS"
@@ -57,25 +57,25 @@ resource "aws_lb_listener" "alb_http_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
-}*/
+}
 
-/*resource "aws_lb_target_group_attachment" "web-attachment" {
+resource "aws_lb_target_group_attachment" "web-attachment" {
   target_group_arn = aws_lb_target_group.alb_target_group.arn
   target_id        = aws_instance.PublicWebTemplate.id
   port             = 80
-  */
-
-# depends_on = [
-#   aws_instance.web-instance
-# ]
 
 
-/*resource "aws_lb_listener" "web-listener" {
-#  load_balancer_arn = aws_lb.web-load-balancer.arn
-#  port              = 80
-#  protocol          = "http"
+  depends_on = [
+  aws_instance.web-instance
+  ]
+}
+
+resource "aws_lb_listener" "web-listener" {
+  load_balancer_arn = aws_lb.web-load-balancer.arn
+  port              = 80
+  protocol          = "http"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web-tg.arn
   }
-}*/
+}
